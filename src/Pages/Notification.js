28b65f443b3624cc1eaf30c34f88775c5f0fe0bc
sindhuja110 <sindhuja1106@gmail.com>
2023-12-13 19:40:28 +0530

    changed the upload file icon and add some color on it  and add accepted file text

--- src/Pages/Notification.js
+++ src/Pages/Notification.js
@@ -1,142 +1,142 @@
-import { Formik, useFormikContext } from "formik";
-import React, { useRef, useState, useCallback } from "react";
-import { Col, Row } from "react-bootstrap";
-import { notifySchema } from "./NotifySchema";
-import { useNavigate } from "react-router-dom";
-import { AiOutlineArrowLeft } from "react-icons/ai";
-import TextArea from "../Components/TextArea";
-import BasicButton from "../Components/BasicButton";
-import TextInput from "../Components/TextInput";
-import { useDropzone } from "react-dropzone";
-import { BsFillCloudUploadFill } from "react-icons/bs";
-import axios from "axios"; // Import Axios
-import { toast } from "react-toastify";
+import { Formik, useFormikContext } from 'formik'
+import React, { useRef, useState, useCallback } from 'react'
+import { Col, Row } from 'react-bootstrap'
+import { notifySchema } from './NotifySchema'
+import { useNavigate } from 'react-router-dom'
+import { AiOutlineArrowLeft } from 'react-icons/ai'
+import TextArea from '../Components/TextArea'
+import BasicButton from '../Components/BasicButton'
+import TextInput from '../Components/TextInput'
+import { useDropzone } from 'react-dropzone'
+import { IoCloudUploadOutline } from 'react-icons/io5'
+import axios from 'axios' // Import Axios
+import { toast } from 'react-toastify'
 
 const Notification = () => {
-  const [title, setTitle] = useState("");
-  const [message, setMessage] = useState("");
-  const [image, setImage] = useState(null);
-  const [linkto, setLinkTo] = useState("");
-  const [imagePreview, setImagePreview] = useState(null);
-  const acceptedImageTypes = ["image/jpeg", "image/png", "image/gif"];
-  const [isSaving, setIsSaving] = useState(false); // New state for loading
+  const [title, setTitle] = useState('')
+  const [message, setMessage] = useState('')
+  const [image, setImage] = useState(null)
+  const [linkto, setLinkTo] = useState('')
+  const [imagePreview, setImagePreview] = useState(null)
+  const acceptedImageTypes = ['image/jpeg', 'image/png', 'image/gif']
+  const [isSaving, setIsSaving] = useState(false) // New state for loading
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
   const handleCancel = () => {
-    navigate("/");
-  };
+    navigate('/')
+  }
 
   const initialValues = {
-    title: "",
-    message: "",
-    linkto: "",
-    image: "",
-  };
+    title: '',
+    message: '',
+    linkto: '',
+    image: '',
+  }
 
-  const fileInputRef = useRef(null);
+  const fileInputRef = useRef(null)
 
   const removeImage = ({ setFieldValue }) => {
     // Remove the selected image
-    setImagePreview(null);
+    setImagePreview(null)
     // Clear the file input value
     if (fileInputRef.current) {
-      fileInputRef.current.value = "";
+      fileInputRef.current.value = ''
     }
     // Clear Formik field
-    setFieldValue("image", "");
-  };
+    setFieldValue('image', '')
+  }
 
   const handleAddData = (values) => {
-    setIsSaving(true);
+    setIsSaving(true)
     const headers = {
-      Authorization: "YourAuthorizationToken", // Replace with your actual token
-      "Content-Type": "application/json",
-    };
+      Authorization: 'YourAuthorizationToken', // Replace with your actual token
+      'Content-Type': 'application/json',
+    }
 
     const data = {
       title: title,
       message: message,
       image: imagePreview,
       link: linkto,
-    };
+    }
 
     axios
       .post(
-        "https://notification-mysql-48f715723b35.herokuapp.com/v1/api/send-push-notification",
+        'https://notification-mysql-48f715723b35.herokuapp.com/v1/api/send-push-notification',
         data,
-        { headers: headers }
+        { headers: headers },
       )
       .then((response) => {
         if (response?.status === 200) {
-          toast.success("Notification sent!", {
-            position: "top-right",
+          toast.success('Notification sent!', {
+            position: 'top-right',
             autoClose: 5000,
             hideProgressBar: false,
             closeOnClick: true,
             pauseOnHover: true,
             draggable: true,
             progress: undefined,
-            theme: "light",
-          });
-          navigate("/");
+            theme: 'light',
+          })
+          navigate('/')
         }
       })
       .catch((error) => {
-        toast.error("Something Went Wrong", {
-          position: "top-right",
+        toast.error('Something Went Wrong', {
+          position: 'top-right',
           autoClose: 5000,
           hideProgressBar: false,
           closeOnClick: true,
           pauseOnHover: true,
           draggable: true,
           progress: undefined,
-          theme: "light",
-        });
+          theme: 'light',
+        })
       })
       .finally(() => {
-        setIsSaving(false);
-      });
-  };
+        setIsSaving(false)
+      })
+  }
 
   const MyDropzone = () => {
-    const { setFieldValue } = useFormikContext();
+    const { setFieldValue } = useFormikContext()
 
     const onDrop = useCallback((acceptedFiles) => {
-      const file = acceptedFiles[0];
-      const reader = new FileReader();
+      const file = acceptedFiles[0]
+      const reader = new FileReader()
 
       reader.onloadend = () => {
-        const img = new Image();
-        img.src = reader.result;
+        const img = new Image()
+        img.src = reader.result
 
         img.onload = () => {
-          const canvas = document.createElement("canvas");
-          const ctx = canvas.getContext("2d");
+          const canvas = document.createElement('canvas')
+          const ctx = canvas.getContext('2d')
 
           // Set the canvas dimensions to the desired width and height (50x50)
-          canvas.width = 150;
-          canvas.height = 150;
+          canvas.width = 150
+          canvas.height = 150
 
           // Draw the image onto the canvas with the desired dimensions
-          ctx.drawImage(img, 0, 0, 150, 150);
+          ctx.drawImage(img, 0, 0, 150, 150)
 
           // Get the data URL of the resized image
-          const resizedImage = canvas.toDataURL("image/jpeg"); // Adjust format if needed
+          const resizedImage = canvas.toDataURL('image/jpeg') // Adjust format if needed
 
           // Set image preview and update Formik field
-          setImagePreview(resizedImage);
-          setFieldValue("image", resizedImage);
-        };
-      };
+          setImagePreview(resizedImage)
+          setFieldValue('image', resizedImage)
+        }
+      }
 
-      reader.readAsDataURL(file);
-    }, []);
+      reader.readAsDataURL(file)
+    }, [])
 
     const { getRootProps, getInputProps, isDragActive } = useDropzone({
       onDrop,
-      accept: acceptedImageTypes.join(","),
+      accept: acceptedImageTypes.join(','),
       multiple: false, // Allow only one file to be dropped
-    });
+    })
 
     return (
       <div className="box-custom" {...getRootProps()}>
@@ -145,16 +145,18 @@ const Notification = () => {
           <p>Drop the files here ...</p>
         ) : (
           <div>
-            <BsFillCloudUploadFill
-              size={25}
-              style={{ cursor: "pointer", color: "black" }}
+            <IoCloudUploadOutline
+              size={40}
+              style={{ cursor: 'pointer', color: 'blue' }}
             />
-            <p>Drag 'n' drop, or click to select files</p>
+            <p className="text-secondary">
+              Drag 'n' drop, or click to select files
+            </p>
           </div>
         )}
       </div>
-    );
-  };
+    )
+  }
 
   return (
     <div className="mr-10 container-fluid">
@@ -193,12 +195,12 @@ const Notification = () => {
                 <BasicButton
                   className="m-1 mb-4"
                   type="submit"
-                  label={isSaving ? "Saving..." : "Save"}
+                  label={isSaving ? 'Saving...' : 'Save'}
                   onClick={
-                    title === "" ||
-                    message === "" ||
-                    linkto === "" ||
-                    image === "" ||
+                    title === '' ||
+                    message === '' ||
+                    linkto === '' ||
+                    image === '' ||
                     (touched.title && errors.title) ||
                     (touched.message && errors.message) ||
                     (touched.linkto && errors.linkto) ||
@@ -212,77 +214,83 @@ const Notification = () => {
 
             <Row className="d-flex flex-wrap flex-lg-row flex-xxl-row flex-xl-row flex-column flex-md-column flex-sm-column mx-2 my-2 ">
               <Col className="m-0 p-4 d-flex flex-wrap flex-column align-items-start justify-content-start shadow rounded bg-white">
-                <Col className="col-12" lg="5" xl="5" md="12" sm="12">
+                <Col className="col-12 mt-1 " lg="5" xl="5" md="12" sm="12">
+                  <Col className="mt-2" />
                   <TextInput
-                    name="title" 
+                    name="title"
                     label="Title"
                     onChange={(e) => {
-                      setTitle(e.target.value);
-                      handleChange(e);
+                      setTitle(e.target.value)
+                      handleChange(e)
                     }}
                     onBlur={handleBlur}
                     placeholder="Enter title here.. "
-                    className={` input ${
-                      touched.title && errors.title ? "is-invalid" : ""
+                    className={`input ${
+                      touched.title && errors.title ? 'is-invalid' : ''
                     }`}
                     validation={
                       touched.title && errors.title ? (
                         <p className="text-danger">{errors.title}</p>
                       ) : (
-                        ""
+                        ''
                       )
                     }
                   />
+                  <Col className="mt-2" />
                   <TextArea
                     name="message"
                     label="Message"
                     onChange={(e) => {
-                      setMessage(e.target.value);
-                      handleChange(e);
+                      setMessage(e.target.value)
+                      handleChange(e)
                     }}
                     onBlur={handleBlur}
                     placeholder="Enter message here.. "
-                    className={` input ${
-                      touched.message && errors.message ? "is-invalid" : ""
+                    className={`  input ${
+                      touched.message && errors.message ? 'is-invalid' : ''
                     }`}
                     validation={
                       touched.message && errors.message ? (
                         <p className="text-danger">{errors.message}</p>
                       ) : (
-                        ""
+                        ''
                       )
                     }
                   />
 
+                  <Col className="mt-2" />
                   <TextInput
                     name="linkto"
                     label="Link "
                     onChange={(e) => {
-                      setLinkTo(e.target.value);
-                      handleChange(e);
+                      setLinkTo(e.target.value)
+                      handleChange(e)
                     }}
                     onBlur={handleBlur}
                     placeholder="Enter link here.. "
                     className={` input ${
-                      touched.linkto && errors.linkto ? "is-invalid" : ""
+                      touched.linkto && errors.linkto ? 'is-invalid' : ''
                     }`}
                     validation={
                       touched.linkto && errors.linkto ? (
                         <p className="text-danger">{errors.linkto}</p>
                       ) : (
-                        ""
+                        ''
                       )
                     }
                   />
-                  <p>
+                  <p className="mt-2">
                     Image Upload
                     <span className={`text-danger`}>*</span>
                   </p>
-                  <p className="text-info mt-2">Image Size: 150x150 pixels</p>
+                  <p className=" text-center ">Image Size: 150x150 pixels </p>
                   <Col>
                     <MyDropzone />
                   </Col>
-
+                  <p className="text-primary text-center m-2">
+                    {' '}
+                    Only .jpeg / .png / .jpg files are accepted
+                  </p>
                   <p>
                     {touched.image && errors.image && (
                       <p className="text-danger">{errors.image}</p>
@@ -294,7 +302,7 @@ const Notification = () => {
                         <img
                           src={imagePreview}
                           alt="Img Preview"
-                          style={{ maxWidth: "100%", marginTop: "10px" }}
+                          style={{ maxWidth: '100%', marginTop: '10px' }}
                         />
                         <p
                           className="text-success mt-2 pointer"
@@ -324,10 +332,10 @@ const Notification = () => {
                   type="submit"
                   label="Save"
                   onClick={
-                    title === "" ||
-                    message === "" ||
-                    linkto === "" ||
-                    image === "" ||
+                    title === '' ||
+                    message === '' ||
+                    linkto === '' ||
+                    image === '' ||
                     (touched.title && errors.title) ||
                     (touched.message && errors.message) ||
                     (touched.linkto && errors.linkto) ||
@@ -342,7 +350,7 @@ const Notification = () => {
         )}
       </Formik>
     </div>
-  );
-};
+  )
+}
 
-export default Notification;
+export default Notification
