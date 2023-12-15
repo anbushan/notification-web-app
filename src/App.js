4f8d25ac3006b02f49e7a11f8ab86e6100da0504
sindhuja110 <sindhuja1106@gmail.com>
2023-12-15 17:46:40 +0530

    add the i18n file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css'
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { Container, Row, Col } from 'react-bootstrap'
-import Notification from './Pages/Notification'
-import Sidebar from './Pages/Sidebar/Sidebar'
-import Products from './Pages/Products'
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import Notification from "./Pages/Notification";
+import Sidebar from "./Pages/Sidebar/Sidebar";
+import Products from "./Pages/Products";
+import LanguageDropdown from "./Components/Language";
 
 function App() {
   return (
@@ -13,16 +14,17 @@ function App() {
         <Container
           fluid
           style={{
-            backgroundColor: '#F5F6FA',
-            height: '100vh',
-            width: '100%',
-            overflowY: 'auto',
+            backgroundColor: "#F5F6FA",
+            height: "100vh",
+            width: "100%",
+            overflowY: "auto",
           }}
         >
           <Row>
             <Col lg={3} xxl={2} xl={2} xs={12}>
               <Sidebar />
             </Col>
+            
 
             <Col lg={9} xxl={10} xl={10}>
               <Routes>
@@ -31,11 +33,12 @@ function App() {
                 <Route path="/notify-add" element={<Notification />} />
               </Routes>
             </Col>
+            
           </Row>
         </Container>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
 
 import App from './App'
+import { LanguageProvider } from './context/LanguageContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
+    <LanguageProvider>
     <App />
     <ToastContainer />
+    </LanguageProvider> 
   </React.StrictMode>,
 )
 
