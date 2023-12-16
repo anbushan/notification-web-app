8bacfc8c4227636b66424f5c8881ae1eb926eb94
sindhuja110 <sindhuja1106@gmail.com>
2023-12-16 19:15:32 +0530

    added the setting sidebar data

--- src/Pages/Sidebar/SidebarData.js
+++ src/Pages/Sidebar/SidebarData.js
@@ -1,12 +1,20 @@
-import { MdOutlineEditNotifications } from 'react-icons/md'
+import { MdOutlineEditNotifications ,MdOutlineSettings } from 'react-icons/md'
 
 export const sidebarItems = [
   {
     id: 1,
     label: 'Notification',
     parent_id: null,
-    icon: <MdOutlineEditNotifications size={25} />,
+    icon: <MdOutlineEditNotifications size={27} />,
     order_index: 2,
     url: '/',
+  },
+  {
+    id: 2,
+    label: 'Settings',
+    parent_id: null,
+    icon: <MdOutlineSettings  size={27} />,
+    order_index: 3,
+    url: '/settings',
   },
 ]
