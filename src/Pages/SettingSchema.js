04f23e5435ab0610cc41f98ca678b4351546e927
sindhuja110 <sindhuja1106@gmail.com>
2023-12-19 19:13:39 +0530

    added the validations for the fields

--- src/Pages/SettingSchema.js
+++ src/Pages/SettingSchema.js
@@ -3,8 +3,20 @@ const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/
 
 export const SettingSchema = yup.object().shape({
     contact: yup.mixed().required('Contact must be required'),
-    sharelink: yup
+    sharelinkAd: yup
     .string()
     .matches(urlRegex, 'URL is not valid')
-    .required('Must be Share your Link'),
+    .required('Must be Share your Android Link'),
+    sharelinkIos: yup
+    .string()
+    .matches(urlRegex, 'URL is not valid')
+    .required('Must be Share your IOS Link'),
+    rateUsAd: yup
+    .string()
+    .matches(urlRegex, 'URL is not valid')
+    .required('Please enter your Android Rate Link'),
+    rateUsIos: yup
+    .string()
+    .matches(urlRegex, 'URL is not valid')
+    .required('Please enter your IOS Rate Link'),
 })
